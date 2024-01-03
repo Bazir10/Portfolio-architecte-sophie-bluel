c7952feed8f38c0ec8b4a7ac7a7cf679dfff82b9
Bazir10 <enydra10@gmail.com>
2024-01-03 03:19:17 +0100

    get et apple api

--- Backend/script.js
+++ Backend/script.js
@@ -1,5 +1,4 @@
-const { response } = require("express");
 
-fetch("http://localhost:5678/api/") 
+fetch("http://localhost:5678/api") 
 .then(response => response.json())
 .then(reponse2 => console.log(reponse2))
